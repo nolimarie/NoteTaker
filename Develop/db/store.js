cd3f9492357689981cddf43ba8dd193335607f1e
agate <nolimarie@protonmail.com>
2021-12-11 03:35:22 -0700

    updated store and apiRoutes files

--- Develop/db/store.js
+++ Develop/db/store.js
@@ -2,7 +2,9 @@
 
 //native node module, that allows us access to the file system, readFile, writeFile
 const fs = require("fs");
+const { getEnabledCategories } = require('trace_events');
 const util = require("util");
+
 
 //we now can use promise objects instead of callbacks
 const readFileAsync = util.promisify(fs.readFile);
@@ -14,23 +16,34 @@ class Store {
     read() {
         return readFileAsync("db/db.json", "utf8");
     }
+    write(note) {
+        return writeFileAsync("db/db.json", JSON.stringify(note))
+    }
 
     getNotes() {
         return this.read().then((notes) => {
             //return the parsedNotes
-            const parsedNotes = JSON.parse(data);
+            const parsedNotes = JSON.parse(notes);
             parsedNotes.push(newNote);
         });
     }
 
     addNote(note) {
         //this will be code to add a note to db.json
-        return writeFileAsync("db/db.json", "utf8");
+        const {title, text} = note
+
+        if (!title || !text) {
+            throw new Error("title and text cannot be blank")
+        }
+
+        const newNote = {title, text}
+
+        return this.getNotes()
+            .then(notes => [...notes, newNote])
+            .then(updatedNotes => this.write(updatedNotes))
+            .then(() => this.newNote)
     }
-
-    //write the rest of your methods in this manner
 }
-
+ 
 //export a newly created object with the methods on the prototype
-
 module.exports = new Store(); //exporting a newly create object from calling the Store class
